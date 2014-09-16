0d7361871b69305dc900644052f08d3920eb3548
Sebastien Varrette <sebastien.varrette@uni.lu>
2014-09-16 16:07:34 +0200

    add betterdefaults
    Signed-off-by: Sebastien Varrette <sebastien.varrette@uni.lu>

--- init.el
+++ init.el
@@ -1,6 +1,6 @@
 ;; -------------------------------------------------------------------------
 ;; .emacs -- my personnal Emacs Init File
-;;           see http://github.com/Falkor/emacs-config
+;;           see http://github.com/Falkor/emacs-config2
 ;;
 ;;       _____     _ _              _
 ;;      |  ___|_ _| | | _____  _ __( )___     ___ _ __ ___   __ _  ___ ___
