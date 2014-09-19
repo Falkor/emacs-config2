481a6973468ddd30269708210c210d232c68970a
Sebastien Varrette <sebastien.varrette@uni.lu>
2014-09-19 16:11:22 +0200

    add custom management
    Signed-off-by: Sebastien Varrette <sebastien.varrette@uni.lu>

--- init.el
+++ init.el
@@ -1,5 +1,5 @@
 ;; -------------------------------------------------------------------------
-;; Time-stamp: <Ven 2014-09-19 13:57 svarrette>
+;; Time-stamp: <Ven 2014-09-19 16:06 svarrette>
 ;;
 ;; .emacs -- my personnal Emacs Init File -- see http://github.com/Falkor/emacs-config2
 ;;
@@ -27,6 +27,12 @@
 (setq is-mac (equal system-type 'darwin))
 
 ;; === Environement settings ===
+
+;; Helper function for root path
+(defun get-conf-path(path)
+  "Appends argument at the end of emacs-root using expand-file-name"
+  (expand-file-name path emacs-root))
+
 ;; init PATH & exec-path from current shell
 (defun set-exec-path-from-shell-PATH ()
   (let ((path-from-shell (shell-command-to-string "$SHELL -c 'echo $PATH'")))
@@ -34,19 +40,19 @@
     (setq exec-path (split-string path-from-shell path-separator))))
 (when window-system (set-exec-path-from-shell-PATH))
 
-;; easy way to load recursively all .el files
-(defun load-directory (directory)
-  "Load recursively all `.el' files in DIRECTORY."
-  (dolist (element (directory-files-and-attributes directory nil nil nil))
-    (let* ((path (car element))
-           (fullpath (concat directory "/" path))
-           (isdir (car (cdr element)))
-           (ignore-dir (or (string= path ".") (string= path ".."))))
-      (cond
-       ((and (eq isdir t) (not ignore-dir))
-        (load-directory fullpath))
-       ((and (eq isdir nil) (string= (substring path -3) ".el"))
-        (load (file-name-sans-extension fullpath)))))))
+;; ;; easy way to load recursively all .el files
+;; (defun load-directory (directory)
+;;   "Load recursively all `.el' files in DIRECTORY."
+;;   (dolist (element (directory-files-and-attributes directory nil nil nil))
+;;     (let* ((path (car element))
+;;            (fullpath (concat directory "/" path))
+;;            (isdir (car (cdr element)))
+;;            (ignore-dir (or (string= path ".") (string= path ".."))))
+;;       (cond
+;;        ((and (eq isdir t) (not ignore-dir))
+;;         (load-directory fullpath))
+;;        ((and (eq isdir nil) (string= (substring path -3) ".el"))
+;;         (load (file-name-sans-extension fullpath)))))))
 
 ;; ============================ Let's go! ============================
 ;; Turn off mouse interface early in startup to avoid momentary display
@@ -65,15 +71,30 @@
   (add-path "site-lisp")
   )
 ;;(add-path "site-lisp/use-package")
-(setq config-dir       (concat emacs-root  "config/"))
-(setq defuns-dir       (concat emacs-root  "defuns"))
-(setq packages-dir     (concat emacs-root  "packages/"))
-(setq custom-file      (concat emacs-root  "custom.el"))
-(setq custom-dir       (concat emacs-root  "rc.custom/"))
+(setq config-dir     (get-conf-path "config/"))
+(setq defuns-dir     (get-conf-path "defuns"))
+(setq packages-dir   (get-conf-path "packages/"))
+(setq custom-file    (get-conf-path "custom.el"))
+(setq custom-dir     (get-conf-path  "rc.custom/"))
 
+;; Helper function to access custom files
+(defun get-custom-path(path)
+  "Appends argument at the end of custom-dir using expand-file-name"
+  (expand-file-name path custom-dir))
 
+;; === setup and evantiually install [missing] packages ===
+;; load falkor/custom/packages is existing
+(setq custom-package-file (get-custom-path "packages.el"))
+(if (file-readable-p custom-package-file)
+	(load-file custom-package-file))
+
+;; Now load/setup packages
+(load-file (get-conf-path "packages.el"))
+
+(require 'load-dir)
 ;; Load Lisp defined functions
-(load-directory defuns-dir)
+(load-dir-one defuns-dir)
+
 
 ;; === Special Mac OS X configuration (Carbon emacs and aquamacs)
 ;; Enhanced Carbon EMacs (ECE) plugin
@@ -92,147 +113,36 @@
 ;;    (load-local-site-start "/Library/Application Support/emacs/ec-emacs/site-lisp")))
 
 
-;; === ELPA, the package manager ===
-;; see http://tromey.com/elpa/
-;; The code below is no longer required on Emacs 24
-;; (when
-;;     (load
-;;      (expand-file-name "~/.emacs.d/elpa/package.el"))
-;;   (package-initialize)
-;;   (require 'init-elpa))
-(require 'package)
-(setq package-archives '(
-                         ("gnu"       . "http://elpa.gnu.org/packages/")
-                         ("melpa"     . "http://melpa.milkbox.net/packages/")
-                                        ;("marmalade" . "http://marmalade-repo.org/packages/")
-                         ))
-(setq package-user-dir  (concat packages-dir "elpa/"))
-(package-initialize)
-
-(defvar falkor/packages '(alert
-                          apache-mode
-                          auto-complete
-                          autopair
-                          better-defaults
-                          color-theme
-                          deft
-                          el-get
-                          erlang
-                          feature-mode
-                          fit-frame
-                          find-file-in-project
-                          flycheck
-                          gist
-                          go-mode
-                          graphviz-dot-mode
-                          haml-mode
-                          haskell-mode
-                          helm
-                          htmlize
-                          idle-require
-                          idle-highlight
-                          jabber
-                          js2-mode
-                          magit
-                          markdown-mode
-                          marmalade
-                          mic-paren
-                          neotree
-                          nodejs-repl
-                          org
-                          paredit
-                          php-mode
-                          powerline
-                          puppet-mode
-                          restclient
-                          ruby-compilation
-                          rvm
-                          smart-tab
-                          smart-tabs-mode
-                          smex
-                          solarized-theme
-			  use-package
-                          web-mode
-                          yaml-mode
-                          yasnippet)
-  "Default packages")
-
-;; Now install the default packages
-(defun falkor/packages-installed-p ()
-  (loop for pkg in falkor/packages
-        when (not (package-installed-p pkg)) do (return nil)
-        finally (return t)))
-
-(unless (falkor/packages-installed-p)
-  (message "%s" "Refreshing package database...")
-  (package-refresh-contents)
-  (dolist (pkg falkor/packages falkor/custom/packages)
-    (when (not (package-installed-p pkg))
-      (package-install pkg))))
 
 
-;; =========== EL-Get =============
-;; See https://github.com/dimitri/el-get
-(unless (require 'el-get nil 'noerror)
-  (with-current-buffer
-      (url-retrieve-synchronously
-       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
-    (let (el-get-master-branch)
-      (goto-char (point-max))
-      (eval-print-last-sexp))))
+;; ;; ======= Enhance initialization speed =======
+;; ;; Some features are not loaded by default to minimize initialization time, so
+;; ;; they have to be required (or loaded, if you will). require-calls tends to
+;; ;; lead to the largest bottleneck's in a configuration. idle-require delays the
+;; ;; require-calls to a time where Emacs is in idle. So this is great for stuff
+;; ;; you eventually want to load, but is not a high priority.
 
+;; (require 'idle-require)             ; Need in order to use idle-require
+;; (dolist (feature
+;;          '(alert
+;;            apache-mode
+;;            auto-compile             ; auto-compile .el files
+;;            deft
+;;            erlang
+;;            gist
+;;            go-mode
+;;            gnuplot
+;;            haml-mode
+;;            jabber
+;;            recentf                  ; recently opened files
+;;            restclient
+;;            smex                     ; M-x interface Ido-style.
+;;            tex-mode
+;;            webgen-mode))               ; TeX, LaTeX, and SliTeX mode commands
+;;   (idle-require feature))
 
-(setq el-get-dir (concat packages-dir "el-get/"))
-(setq el-get-status-file   (concat el-get-dir ".status.el"))
-(setq el-get-autoload-file (concat el-get-dir ".loaddefs.el"))
-
-(require 'el-get)
-
-;;(setq el-get-byte-compile nil)
-;; Load the local recipes
-(add-to-list 'el-get-recipe-path (concat el-get-dir "recipes"))
-
-;; ECB for emacs 24
-(setq el-get-sources
-      '((:name ecb
-               :type git
-               :url "https://github.com/alexott/ecb.git"
-               :load "ecb.el"
-               :compile ("ecb.el"))
-        ))
-(setq my-packages
-      (append '(el-get)
-              (mapcar 'el-get-source-name el-get-sources)))
-(el-get 'sync my-packages)
-
-;; ======= Enhance initialization speed =======
-;; Some features are not loaded by default to minimize initialization time, so
-;; they have to be required (or loaded, if you will). require-calls tends to
-;; lead to the largest bottleneck's in a configuration. idle-require delays the
-;; require-calls to a time where Emacs is in idle. So this is great for stuff
-;; you eventually want to load, but is not a high priority.
-
-(require 'idle-require)             ; Need in order to use idle-require
-(dolist (feature
-         '(alert
-           apache-mode
-           auto-compile             ; auto-compile .el files
-           deft
-           erlang
-           gist
-           go-mode
-           gnuplot
-           haml-mode
-           jabber
-           recentf                  ; recently opened files
-           restclient
-           smex                     ; M-x interface Ido-style.
-           tex-mode
-           webgen-mode))               ; TeX, LaTeX, and SliTeX mode commands
-  (idle-require feature))
-
-(setq idle-require-idle-delay 5)
-(idle-require-mode 1)
+;; (setq idle-require-idle-delay 5)
+;; (idle-require-mode 1)
 
 
 
@@ -244,5 +154,5 @@
 
 ;; ===== Custom settings ====
 ;; Overwrite with the custom settings
-;;(load-directory custom-dir)
+(load-dir-one custom-dir)
 (load custom-file 'noerror)
---
+++ packages.el
@@ -0,0 +1,135 @@
+;; -*- mode: lisp; -*-
+;;
+;; Load package managers -- assumes you setup the variable packages-dir as the
+;; root directory to host your packages. Then
+;; * ELPA packages will be hosted in <packages-dir>/elpa/
+;; * EL-get packages will be hosted in <packages-dir>/el-get/
+;;
+
+(require 'cl)
+
+;; === ELPA, the package manager ===
+;; see http://tromey.com/elpa/
+;; The code below is no longer required on Emacs 24
+;; (when
+;;     (load
+;;      (expand-file-name "~/.emacs.d/elpa/package.el"))
+;;   (package-initialize)
+;;   (require 'init-elpa))
+(require 'package)
+
+
+;; === setup packages sources ===
+(setq package-archives '(
+                         ("gnu"       . "http://elpa.gnu.org/packages/")
+                         ("melpa"     . "http://melpa.milkbox.net/packages/")
+                                        ;("marmalade" . "http://marmalade-repo.org/packages/")
+                         ))
+(setq package-user-dir  (concat packages-dir "elpa/"))
+(package-initialize)
+
+
+;;
+;; Define packages to install
+;;
+(defvar falkor/packages '(alert
+                          apache-mode
+                          auto-complete
+                          autopair
+                          better-defaults
+                          color-theme
+                          deft
+                          el-get
+                          erlang
+                          feature-mode
+                          fit-frame
+                          find-file-in-project
+                          flycheck
+                          gist
+                          go-mode
+                          graphviz-dot-mode
+                          haml-mode
+                          haskell-mode
+                          helm
+                          htmlize
+                          idle-require
+                          idle-highlight
+                          jabber
+                          js2-mode
+                          load-dir
+                          magit
+                          markdown-mode
+                          marmalade
+                          mic-paren
+                          neotree
+                          nodejs-repl
+                          org
+                          paredit
+                          php-mode
+                          powerline
+                          puppet-mode
+                          restclient
+                          ruby-compilation
+                          rvm
+                          smart-tab
+                          smart-tabs-mode
+                          smex
+                          solarized-theme
+                          use-package
+                          web-mode
+                          yaml-mode
+                          yasnippet)
+  "Default packages")
+
+;; Eventually load custom configuration
+;; (defvar falkor/custom/packages 
+;;   nil
+;;   "Custom List of packages")
+
+;; Now install the default packages
+(defun falkor/packages-installed-p ()
+  (loop for pkg in '(append '(falkor/packages) '(falkor/custom/packages))
+        when (not (package-installed-p pkg)) do (return nil)
+        finally (return t)))
+
+(unless (falkor/packages-installed-p)
+  (message "%s" "Refreshing package database...")
+  (package-refresh-contents)
+  (dolist (pkg falkor/packages falkor/custom/packages)
+    (when (not (package-installed-p pkg))
+      (package-install pkg))))
+
+
+;; =========== EL-Get =============
+;; See https://github.com/dimitri/el-get
+(unless (require 'el-get nil 'noerror)
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
+    (let (el-get-master-branch)
+      (goto-char (point-max))
+      (eval-print-last-sexp))))
+
+
+(setq el-get-dir (concat packages-dir "el-get/"))
+(setq el-get-status-file   (concat el-get-dir ".status.el"))
+(setq el-get-autoload-file (concat el-get-dir ".loaddefs.el"))
+
+(require 'el-get)
+
+;;(setq el-get-byte-compile nil)
+;; Load the local recipes
+(add-to-list 'el-get-recipe-path (concat el-get-dir "recipes"))
+
+;; ECB for emacs 24
+(setq el-get-sources
+      '((:name ecb
+               :type git
+               :url "https://github.com/alexott/ecb.git"
+               :load "ecb.el"
+               :compile ("ecb.el"))
+        ))
+(setq my-packages
+      (append '(el-get)
+              (mapcar 'el-get-source-name el-get-sources)))
+(el-get 'sync my-packages)
