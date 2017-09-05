57ee55e31ed1c2d26b227d76e0f57092ab400a3c
Sebastien Varrette <Sebastien.Varrette@uni.lu>
2017-09-05 09:29:15 +0200

    update customs
    Signed-off-by: Sebastien Varrette <Sebastien.Varrette@uni.lu>

--- custom.el
+++ custom.el
@@ -3,9 +3,16 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(neo-hidden-regexp-list
+   (quote
+    ("^\\." "\\.pyc$" "~$" "^#.*#$" "\\.elc$" "\\.synctex\\.gz$" "\\.md\\.tex$")))
+ '(neo-window-fixed-size nil)
+ '(neo-window-width 35)
  '(package-selected-packages
    (quote
-    (flycheck-rtags company-rtags yaml-mode ws-butler web-mode vimrc-mode use-package sublimity sr-speedbar spaceline solarized-theme smex smartparens smart-tabs-mode smart-tab smart-mode-line smart-compile rvm ruby-compilation rtags robe restclient restart-emacs puppet-mode php-mode paredit pandoc-mode pabbrev osx-plist nodejs-repl neotree multiple-cursors minimap mic-paren marmalade markdown-toc magit-gitflow lua-mode load-dir latex-extra json-mode js2-mode indent-guide htmlize helm-swoop helm-projectile helm-package helm-gtags helm-company helm-c-yasnippet guide-key-tip gnuplot-mode gitconfig-mode git-timemachine git-gutter-fringe ggtags function-args flycheck-irony fit-frame find-file-in-project feature-mode f expand-region exec-path-from-shell enh-ruby-mode doom-themes company-irony-c-headers company-irony company-c-headers company-auctex color-theme-modern color-theme cmake-mode cmake-ide bury-successful-compilation better-defaults bats-mode autopair auto-dictionary auto-compile apache-mode alert airline-themes ace-jump-mode))))
+    (flycheck-rtags company-rtags yaml-mode ws-butler web-mode vimrc-mode use-package sublimity sr-speedbar spaceline solarized-theme smex smartparens smart-tabs-mode smart-tab smart-mode-line smart-compile rvm ruby-compilation rtags robe restclient restart-emacs puppet-mode php-mode paredit pandoc-mode pabbrev osx-plist nodejs-repl neotree multiple-cursors minimap mic-paren marmalade markdown-toc magit-gitflow lua-mode load-dir latex-extra json-mode js2-mode indent-guide htmlize helm-swoop helm-projectile helm-package helm-gtags helm-company helm-c-yasnippet guide-key-tip gnuplot-mode gitconfig-mode git-timemachine git-gutter-fringe ggtags function-args flycheck-irony fit-frame find-file-in-project feature-mode f expand-region exec-path-from-shell enh-ruby-mode doom-themes company-irony-c-headers company-irony company-c-headers company-auctex color-theme-modern color-theme cmake-mode cmake-ide bury-successful-compilation better-defaults bats-mode autopair auto-dictionary auto-compile apache-mode alert airline-themes ace-jump-mode)))
+ '(scroll-bar-mode nil)
+ '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
